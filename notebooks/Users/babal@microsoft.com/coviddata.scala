57aea4eae5b1c80d5403c0c26dce911c5862d798
Bala <bala_b@hotmail.com>
2020-10-06 17:10:03 -0500

    Update: .gitignore GBHD README.md Synapse adbcapstone adf aml cap notebooks

---
+++ notebooks/Users/babal@microsoft.com/coviddata.scala
@@ -0,0 +1,31 @@
+// Databricks notebook source
+spark.conf.set(
+  "fs.azure.account.key.accbbstore.blob.core.windows.net",
+  "iRfUM9DFyQKhE9BXbPOlfQIgjuz3GTQ7p0i8Y23Sxwa9CRchLHTPOPQqGrpCd6eQaQ141vc5PBqdrImOwvgXqA==")
+
+// COMMAND ----------
+
+val data = spark.read.option("inferSchema", "true").option("header", "true").csv("wasbs://coviddata@accbbstore.blob.core.windows.net/covid_19_data.csv")
+
+// COMMAND ----------
+
+display(data)
+
+// COMMAND ----------
+
+data.createOrReplaceTempView("coviddata")
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC select * from coviddata limit 10
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC select `Country/Region`, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by `Country/Region` order by `Country/Region`
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC select `Country/Region`, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by `Country/Region` order by `Country/Region`
