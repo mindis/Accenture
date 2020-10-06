fb0776bdcf5d670017cf0447d27d95abf451f964
Bala <bala_b@hotmail.com>
2020-10-06 17:28:02 -0500

    Update: .gitignore GBHD README.md Synapse adbcapstone adf aml cap notebooks

--- notebooks/Users/babal@microsoft.com/coviddata.scala
+++ notebooks/Users/babal@microsoft.com/coviddata.scala
@@ -28,4 +28,13 @@ data.createOrReplaceTempView("coviddata")
 // COMMAND ----------
 
 // MAGIC %sql
-// MAGIC select `Country/Region`, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by `Country/Region` order by `Country/Region`
+// MAGIC select ObservationDate, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by ObservationDate order by ObservationDate
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC select year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "EEEE")) as year, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "EEEE")) order by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "EEEE"))
