4ee742b9a3adc1bf24da42982a5221921977178f
Bala <bala_b@hotmail.com>
2020-10-06 17:52:05 -0500

    Update: .gitignore GBHD README.md Synapse adbcapstone adf aml cap notebooks

--- notebooks/Users/babal@microsoft.com/coviddata.scala
+++ notebooks/Users/babal@microsoft.com/coviddata.scala
@@ -13,6 +13,29 @@ display(data)
 
 // COMMAND ----------
 
+import org.apache.spark.sql.functions
+import org.apache.spark.sql.functions.broadcast
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import scala.collection.mutable.ListBuffer
+import java.util.{GregorianCalendar, Date}
+import java.util.Calendar
+
+// COMMAND ----------
+
+data.select("year", date_format(last_day($"ObservationDate"), "MM/dd/yyyy"))
+
+// COMMAND ----------
+
+val df1 = data.withColumn("year", date_format(to_date($"ObservationDate"), "MM/dd/yyyy"))
+
+// COMMAND ----------
+
+display(df1)
+
+// COMMAND ----------
+
 data.createOrReplaceTempView("coviddata")
 
 // COMMAND ----------
@@ -37,4 +60,10 @@ import org.apache.spark.sql.functions
 // COMMAND ----------
 
 // MAGIC %sql
-// MAGIC select year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "EEEE")) as year, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "EEEE")) order by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "EEEE"))
+// MAGIC select year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")) as year, Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")) order by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y"))
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC select year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")) as year, month(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")) as month, 
+// MAGIC Sum(Confirmed) as Confirmed, Sum(Deaths) as Deaths, Sum(Recovered) as Recovered from coviddata group by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")), month(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")) order by year(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y")), month(date_format(to_date(ObservationDate, "MM/dd/yyyy"), "M/d/y"))
